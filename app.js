07e48166ef268f40f6c0481acf33cfe48ff1a89c
mvvlq <ardenmaalik@gmail.com>
2020-06-10 12:55:26 -0400

    npm start fix

--- app.js
+++ app.js
@@ -3,5 +3,4 @@ const PORT = process.env.PORt || 5000;
 
 express()
 	.use(express.static("public"))
-	// .get("/", (req, res) => res.render('pages/index')
 	.listen(PORT, () => console.log(`Listening on ${PORT} ...`));
